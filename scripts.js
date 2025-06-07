694e9a76d551f3d71176f40d0e68a757ecc8db1b
MashedPotato123 <88502722+MashedPotato123@users.noreply.github.com>
2025-06-07 12:43:24 +0800

    Remove member and slightly change the code
    Remove member and slightly change the code

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ let currentIndex = 0;
 function moveSlide(direction) {
   const itemsPerPage = window.innerWidth <= 600 ? 1 : (window.innerWidth <= 1024 ? 2 : 4);
   const totalItems = carousel.children.length;
-  const maxIndex = Math.ceil(totalItems / itemsPerPage) + 2;
+  const maxIndex = Math.ceil(totalItems / itemsPerPage) + 1;
 
   currentIndex += direction;
 
